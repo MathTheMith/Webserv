5b8511018bb603e46bfebcb368dd0a96a4ddd6da
NoaFanizzi <noafanizzistdy@gmail.com>
2026-05-26 16:48:32 +0200

    one last fix for html

--- srcs/AutoIndex/srcs/AutoIndex.cpp
+++ srcs/AutoIndex/srcs/AutoIndex.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AutoIndex.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nofanizz <nofanizz@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: nofanizz <nofanizz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 15:01:52 by nofanizz          #+#    #+#             */
-/*   Updated: 2026/05/11 12:13:46 by nofanizz         ###   ########.fr       */
+/*   Updated: 2026/05/26 16:47:59 by nofanizz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,80 +20,80 @@
 #include <stdlib.h>
 
 std::string AutoIndex::_header =
-	"<!DOCTYPE html>"
-	"<html lang='en'>"
-	"<head>"
-	"<meta charset='UTF-8'/>"
-	"<title>Index</title>"
-	"<style>"
-	"body {"
-	"  font-family: monospace;"
-	"  background: #1a1a1a;"
-	"  color: #ccc;"
-	"  margin: 0;"
-	"  height: 100vh;"
-	"  display: flex;"
-	"  justify-content: center;"
-	"  align-items: center;"
-	"}"
-	".container {"
-	"  background: #222;"
-	"  padding: 40px;"
-	"  border-radius: 8px;"
-	"  box-shadow: 0 0 25px rgba(0,0,0,0.6);"
-	"  width: 700px;"
-	"  max-width: 90%;"
-	"}"
-	"h1 {"
-	"  color: #fff;"
-	"  font-size: 24px;"
-	"  margin-bottom: 30px;"
-	"  border-bottom: 1px solid #333;"
-	"  padding-bottom: 15px;"
-	"  text-align: center;"
-	"}"
-	"table {"
-	"  width: 100%;"
-	"  border-collapse: collapse;"
-	"}"
-	"th {"
-	"  color: #777;"
-	"  font-size: 13px;"
-	"  text-transform: uppercase;"
-	"  letter-spacing: .1em;"
-	"  padding: 12px 16px;"
-	"  text-align: left;"
-	"}"
-	"td {"
-	"  padding: 14px 16px;"
-	"  border-top: 1px solid #2a2a2a;"
-	"  font-size: 15px;"
-	"}"
-	"a {"
-	"  color: #6af;"
-	"  text-decoration: none;"
-	"}"
-	"a:hover {"
-	"  text-decoration: underline;"
-	"}"
-	".size {"
-	"  color: #777;"
-	"}"
-	".delete-button {"
-	"background-color: red;"
-	"border: none;"
-	"color: white;"
-	"padding: 15px 32px;"
-	"text-align: center;"
-	"text-decoration: none;"
-	"display: inline-block;"
-	"font-size: 16px;"
-	"cursor: pointer;"
-	"}"
-	".dir a { color: #b8d839ff; font-weight: bold; }"
-	".file a { color: #6af; }"
-	"</style>"
-	"<script>"
+		"<!DOCTYPE html>"
+		"<html lang='en'>"
+		"<head>"
+		"<meta charset='UTF-8'/>"
+		"<title>Index</title>"
+		"<style>"
+		"body {"
+		"  font-family: monospace;"
+		"  background: #1a1a1a;"
+		"  color: #ccc;"
+		"  margin: 0;"
+		"  min-height: 100vh;"
+		"  display: flex;"
+		"  justify-content: center;"
+		"  align-items: center;"
+		"}"
+		".container {"
+		"  background: #222;"
+		"  padding: 40px;"
+		"  border-radius: 8px;"
+		"  box-shadow: 0 0 25px rgba(0,0,0,0.6);"
+		"  width: 700px;"
+		"  max-width: 90%;"
+		"}"
+		"h1 {"
+		"  color: #fff;"
+		"  font-size: 24px;"
+		"  margin-bottom: 30px;"
+		"  border-bottom: 1px solid #333;"
+		"  padding-bottom: 15px;"
+		"  text-align: center;"
+		"}"
+		"table {"
+		"  width: 100%;"
+		"  border-collapse: collapse;"
+		"}"
+		"th {"
+		"  color: #777;"
+		"  font-size: 13px;"
+		"  text-transform: uppercase;"
+		"  letter-spacing: .1em;"
+		"  padding: 12px 16px;"
+		"  text-align: left;"
+		"}"
+		"td {"
+		"  padding: 14px 16px;"
+		"  border-top: 1px solid #2a2a2a;"
+		"  font-size: 15px;"
+		"}"
+		"a {"
+		"  color: #6af;"
+		"  text-decoration: none;"
+		"}"
+		"a:hover {"
+		"  text-decoration: underline;"
+		"}"
+		".size {"
+		"  color: #777;"
+		"}"
+		".delete-button {"
+		"background-color: red;"
+		"border: none;"
+		"color: white;"
+		"padding: 15px 32px;"
+		"text-align: center;"
+		"text-decoration: none;"
+		"display: inline-block;"
+		"font-size: 16px;"
+		"cursor: pointer;"
+		"}"
+		".dir a { color: #b8d839ff; font-weight: bold; }"
+		".file a { color: #6af; }"
+		"</style>"
+		"<script>"
 		"document.addEventListener('DOMContentLoaded', () => {"
 		"  const deleteButtons = document.querySelectorAll('.delete-button');"
 		"  deleteButtons.forEach(button => {"
@@ -105,7 +105,7 @@ std::string AutoIndex::_header =
 		"  const id = button.dataset.id;"
 		"const ok = confirm('Are you sure that you want to delete this file ?');"
 		"if (!ok) {"
-			"return;"
+		"return;"
 		"}"
 		"  const response = await fetch(`${id}`, {"
 		"    method: 'DELETE'"
@@ -115,57 +115,54 @@ std::string AutoIndex::_header =
 		" 	 window.location.reload();"
 		"  }"
 		"}"
-	"</script>"
+		"</script>"
 
-
-	"</head>"
-	"<body>"
-	"<div class='container'>"
-	"<h1>Index</h1>"
-	"<table>"
-	"<thead><tr><th>Name</th><th>Date</th><th>Size</th></tr></thead>"
-	"<tbody>";
+		"</head>"
+		"<body>"
+		"<div class='container'>"
+		"<h1>Index</h1>"
+		"<table>"
+		"<thead><tr><th>Name</th><th>Date</th><th>Size</th></tr></thead>"
+		"<tbody>";
 
 std::string AutoIndex::_template =
-	"<tr class='{{ TYPE }}'>"
-	"<td><a href='{{ URL }}'>{{ NAME }}</a></td>"
-	"<td class='size'>{{ DATE }}</td>"
-	"<td class='size'>{{ WEIGHT }}</td>"
-	"<td class='delete-button' data-id='{{ DELETE }}'>DELETE</td>"
-	"</tr>";
+		"<tr class='{{ TYPE }}'>"
+		"<td><a href='{{ URL }}'>{{ NAME }}</a></td>"
+		"<td class='size'>{{ DATE }}</td>"
+		"<td class='size'>{{ WEIGHT }}</td>"
+		"<td class='delete-button' data-id='{{ DELETE }}'>DELETE</td>"
+		"</tr>";
 
 std::string AutoIndex::_footer =
-	"</tbody></table>"
-	"</div>"
-	"</body></html>";
+		"</tbody></table>"
+		"</div>"
+		"</body></html>";
 
-AutoIndex::AutoIndex(const std::string &root, const std::string &location) :
-	_root(root), _location(location) {}
-									
+AutoIndex::AutoIndex(const std::string &root, const std::string &location) : _root(root), _location(location) {}
+
 void AutoIndex::replaceName(std::string &newTemplate, struct dirent &sdir)
 {
 	const std::string name = sdir.d_name;
 	std::string test;
 	newTemplate.find("dir") != std::string::npos ? test = "📁 " : test = "";
-	newTemplate.replace(newTemplate.find("{{ NAME }}"), 10, test +  name);
+	newTemplate.replace(newTemplate.find("{{ NAME }}"), 10, test + name);
 }
-
 
 void AutoIndex::replaceLink(std::string &newTemplate, struct dirent &sdir)
 {
-    std::string fLocation = _location;
-    
-    if (!fLocation.empty() && fLocation[fLocation.length() - 1] != '/')
-        fLocation += "/";
-    
-    fLocation += sdir.d_name;
-    newTemplate.replace(newTemplate.find("{{ URL }}"), 9, fLocation);
-    newTemplate.replace(newTemplate.find("{{ DELETE }}"), 12, fLocation);
+	std::string fLocation = _location;
+
+	if (!fLocation.empty() && fLocation[fLocation.length() - 1] != '/')
+		fLocation += "/";
+
+	fLocation += sdir.d_name;
+	newTemplate.replace(newTemplate.find("{{ URL }}"), 9, fLocation);
+	newTemplate.replace(newTemplate.find("{{ DELETE }}"), 12, fLocation);
 }
 
 void AutoIndex::replaceDate(std::string &newTemplate, struct stat &file)
 {
-	struct tm * timeinfo = std::localtime(&file.st_mtime);
+	struct tm *timeinfo = std::localtime(&file.st_mtime);
 
 	char buffer[80];
 	std::strftime(buffer, 80, "%Y-%m-%d %H:%M", timeinfo);
@@ -191,9 +188,9 @@ std::string AutoIndex::replaceTemplate(struct dirent &sdir, const std::string &r
 	struct stat file;
 
 	std::string filepath = rPath + "/" + sdir.d_name;
-	if(stat(filepath.c_str(), &file) == -1)
+	if (stat(filepath.c_str(), &file) == -1)
 		return "";
-		
+
 	if (S_ISDIR(file.st_mode))
 		newTemplate.replace(newTemplate.find("{{ TYPE }}"), 10, "dir");
 	else
@@ -217,22 +214,25 @@ void AutoIndex::addNewRow(struct dirent &sdir, const std::string &rPath)
 std::string AutoIndex::initAutoIndex(const std::string &rPath)
 {
 	DIR *dr;
-	struct dirent* sdir;
+	struct dirent *sdir;
 
 	dr = opendir(rPath.c_str());
 	if (!dr)
 		throw Http403Exception();
 	_content = _header;
-	try {
-		while((sdir = readdir(dr)) != NULL)
+	try
+	{
+		while ((sdir = readdir(dr)) != NULL)
 		{
 			addNewRow(*sdir, rPath);
 		}
-	} catch (...) { //TODO ... c'est quoi cette merde !
+	}
+	catch (...)
+	{ // TODO ... c'est quoi cette merde !
 		closedir(dr);
 		throw;
 	}
 	closedir(dr);
 	_content.append(_footer);
-	return(_content);
+	return (_content);
 }
