6808f9a3bf8905fec699c2a00538b8398cd11b94
NoaFanizzi <noafanizzistdy@gmail.com>
2026-05-28 15:54:54 +0200

    style : removed some comments

--- srcs/CGI/cgi.mk
+++ srcs/CGI/cgi.mk
@@ -3,17 +3,15 @@
 #                                                         :::      ::::::::    #
 #    cgi.mk                                             :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lmarcucc <lmarcucc@student.42.fr>          +#+  +:+       +#+         #
+#    By: nofanizz <nofanizz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/31 11:14:01 by nofanizz          #+#    #+#              #
-#    Updated: 2026/05/11 15:16:16 by lmarcucc         ###   ########.fr        #
+#    Updated: 2026/05/28 15:54:16 by nofanizz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# Ajout des sources CGI
 SRCS += srcs/CGI/srcs/CgiManager.cpp \
 		srcs/CGI/srcs/CgiWriter.cpp \
 		srcs/CGI/srcs/CgiReader.cpp
 
-# Ajout des includes CGI
 INCLUDES += -Isrcs/CGI/includes
